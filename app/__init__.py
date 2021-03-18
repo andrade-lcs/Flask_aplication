01ed78044f828a3e61cec93913c02c37e8b1694e
andrade-lcs <eng.lucasandrade@gmail.com>
2021-03-18 19:22:24 -0300

    Com modelos de banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLalchemy
+import os
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 from app import routes
+db = SQLalchemy(app)
