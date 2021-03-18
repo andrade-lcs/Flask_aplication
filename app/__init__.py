1c8eb4c4b01e2c78ed15a770325e1996374c18f8
andrade-lcs <eng.lucasandrade@gmail.com>
2021-03-18 19:35:43 -0300

    correções

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLalchemy
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-
-from app import routes
-db = SQLalchemy(app)
+from app import routes, models
+db.creat_all()
